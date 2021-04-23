7eae5db1c18204293b7aa645c3f57205d9ee8bc7
Augusto Silva <augusto_jvs@hotmail.com>
2021-04-23 15:27:25 -0300

    adição da tela de identificação e rotas

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Welcome } from './src/pages/Welcome';
+import Routes from './src/routes';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App(){
@@ -14,7 +14,7 @@ export default function App(){
   }
 
   return (
-    <Welcome />
+    <Routes />
   )
 }
 
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,8 +5,14 @@ import { Feather } from '@expo/vector-icons'
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
 
 export function Welcome(){
+    const navigation = useNavigation()
+
+    function handleStart(){
+        navigation.navigate('UserIdentification');
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -29,8 +35,9 @@ export function Welcome(){
                 </Text>
 
                 <TouchableOpacity 
-                style={styles.button} 
-                activeOpacity={0.7}
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                    onPress={handleStart}
                 >
                     <Feather 
                         name="chevron-right" 
