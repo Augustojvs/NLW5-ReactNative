019e70510f00bf1fdaf65a7325de88d73dd69fe7
Augusto Silva <augusto_jvs@hotmail.com>
2021-05-04 17:25:28 -0300

    criação da listagem de plantas e paginação

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://10.0.0.123:3333',
 });
-
+// json-server ./src/services/server.json --host 10.0.0.123 --port 3333
 export default api;
